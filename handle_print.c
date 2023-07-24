8e17c8e65e9ec779e3d6e56368f730b8294883ea
Veronica Akoth Owich <owichveronica2003@gmail.com>
2023-07-24 12:59:20 -0700

    fixed

--- handle_print.c
+++ handle_print.c
@@ -1,31 +1,31 @@
 #include "main.h"
 /**
-*handle_print - Prints an argument
-*@fmt: Formatted string
-*@list: arguments to be printed.
-*@ind: ind
-*@buffer: Buffer array print
-*@flags: Calculates flags
-*@width: get width
-*@precision:specification
-*@size: Size
+*handle_print - Prints an argument based on its type
+*@fmt: Formatted string in which to print the arguments.
+*@list: List of arguments to be printed.
+*@ind: ind.
+*@buffer: Buffer array to handle print.
+*@flags: Calculates active flags
+*@width: get width.
+*@precision: Precision specification
+*@size: Size specifier
 *Return: 1 or 2;
 */
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 int flags, int width, int precision, int size)
 {
-int v, unknow_len = 0, printed_chars = -1;
+int i, unknow_len = 0, printed_chars = -1;
 fmt_t fmt_types[] = {
 {'c', print_char}, {'s', print_string}, {'%', print_percent},
-{'v', print_int}, {'d', print_int}, {'b', print_binary},
+{'i', print_int}, {'d', print_int}, {'b', print_binary},
 {'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
 {'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
 {'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
 };
-for (v = 0; fmt_types[v].fmt != '\0'; v++)
-if (fmt[*ind] == fmt_types[v].fmt)
-return (fmt_types[v].fn(list, buffer, flags, width, precision, size));
-if (fmt_types[v].fmt == '\0')
+for (i = 0; fmt_types[i].fmt != '\0'; i++)
+if (fmt[*ind] == fmt_types[i].fmt)
+return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
+if (fmt_types[i].fmt == '\0')
 {
 if (fmt[*ind] == '\0')
 return (-1);
